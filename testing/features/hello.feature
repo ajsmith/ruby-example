ba85a15a9a37ac187ccb3ecc13bb1fee18c09cd9
Alex Smith <alex.smith@redhat.com>
2014-07-11 16:00:23 -0400

    Add very basic cucumber tests.

---
+++ testing/features/hello.feature
@@ -0,0 +1,7 @@
+Feature: Hello
+  In order to demonstrate basic testing
+  As a Ruby developer
+  I want to print a string
+
+  Scenario: Say hello
+  Then this feature says hello.
---
+++ testing/features/step_definitions/hello_steps.rb
@@ -0,0 +1,12 @@
+Given /^nothing$/ do
+  # Do nothing
+end
+
+When /^always$/ do
+  # Do nothing
+end
+
+Then /^this feature says hello\.$/ do
+  # puts "Hello!"
+  raise "nope"
+end
